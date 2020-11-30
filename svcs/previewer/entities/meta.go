6758e97a1e8a2dfb07608647ffe5cb817d5f844b
NiponChi <nipon.chi@gmail.com>
2020-11-30 22:39:46 +0700

    foxfox-learn/issues/15 added total chapters in meta

--- svcs/previewer/entities/meta.go
+++ svcs/previewer/entities/meta.go
@@ -5,9 +5,14 @@ import (
 	"time"
 )
 
+type MetaEx struct {
+	*types.Meta
+	TotalChapters int `json:"totalChapters"`
+}
+
 type Meta struct {
-	FileId       string      `json:"fileId"`
-	Revision     int         `json:"revision"`
-	ExportedDate time.Time   `json:"exportedDate"`
-	Meta         *types.Meta `json:"meta"`
+	FileId       string    `json:"fileId"`
+	Revision     int       `json:"revision"`
+	ExportedDate time.Time `json:"exportedDate"`
+	Meta         *MetaEx   `json:"meta"`
 }
